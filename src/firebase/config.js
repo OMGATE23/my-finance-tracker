3e1a54615fee4957768cae886069e4f25548b586
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-26 17:49:58 +0530

    added firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA5u2b6XHYdgt-E0PgEy39foYOPCO9zono",
+    authDomain: "my-finance-tracker-c1768.firebaseapp.com",
+    projectId: "my-finance-tracker-c1768",
+    storageBucket: "my-finance-tracker-c1768.appspot.com",
+    messagingSenderId: "981345891008",
+    appId: "1:981345891008:web:1cf1b47f68846bf75f3325"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+projectFirebase = firebase.firestore()
+projectAuth = firebase.auth()
+
+export {projectFirebase , projectAuth}
