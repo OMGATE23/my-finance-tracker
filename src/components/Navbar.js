f91afc0055e38641e25d1451d8af8e29184cf1b5
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-29 17:17:04 +0530

    feat: included logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 import styles from './navbar.module.css'
 
 export default function Navbar() {
+  const {logout} = useLogout()
   return (
     <nav>
         
@@ -15,6 +17,10 @@ export default function Navbar() {
 
             <li>
                 <Link to="/signup">Signup</Link>
+            </li>
+
+            <li>
+              <button className='btn' onClick={logout}>logout</button>
             </li>
         </ul>
     </nav>
