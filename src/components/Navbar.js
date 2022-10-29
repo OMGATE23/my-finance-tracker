20fadf4f2518409fc8c9b4ad829125b25d79264b
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-29 21:48:33 +0530

    feat: conditional renderring of nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
 import { useLogout } from '../hooks/useLogout'
 import styles from './navbar.module.css'
 
 export default function Navbar() {
   const {logout} = useLogout()
+  const {user} = useAuthContext()
+
   return (
     <nav>
         
         <ul className={styles['nav-list']}>
 
             <li><h1 className={styles['nav-header']}>My Finance Tracker</h1></li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
+            
+            {!user && (
+              <>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
 
-            <li>
-                <Link to="/signup">Signup</Link>
-            </li>
+                <li>
+                    <Link to="/signup">Signup</Link>
+                </li>
+              </>
+            )}
 
-            <li>
-              <button className='btn' onClick={logout}>logout</button>
-            </li>
+            {user && (
+              <>
+                <li>Welcome {user.displayName}!</li>
+                <li>
+                  <button className='btn' onClick={logout}>logout</button>
+                </li>
+              </>
+            )}
         </ul>
     </nav>
   )
