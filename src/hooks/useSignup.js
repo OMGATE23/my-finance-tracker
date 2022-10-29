fecae278de076c03f376cbf04e295771418683d1
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-29 21:50:04 +0530

    feat: added cleanup function

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { projectAuth } from '../firebase/config'
 import {useAuthContext} from './useAuthContext'
@@ -8,6 +8,7 @@ export default function useSignup() {
     const [error, setError] = useState(null)
     const [isPending, setIsPending] = useState(false)
     const {dispatch} = useAuthContext()
+    const [isCancelled, setIsCancelled] = useState(false)
 
     const signup = async(email, password, displayName) => {
         setError(null)
@@ -28,13 +29,26 @@ export default function useSignup() {
             })
 
             await res.user.updateProfile({displayName})
-            setIsPending(false)
+
+            if(!isCancelled){
+                setIsPending(false)
+                setError(null)
+            }
+            
 
         } catch(err){
-            console.error(err.message)
-            setError(err.message)
-            setIsPending(false)
+
+            if(!isCancelled){
+                console.error(err.message)
+                setError(err.message)
+                setIsPending(false)
+            }
+            
         }
     }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    } , [])
   return {error , isPending, signup}
 }
