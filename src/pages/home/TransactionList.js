3381ca31c8eba7cb9a1f8f8ddaf31f8f8e352a28
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-31 22:00:03 +0530

    feat: added delete document function

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import useFirestore from '../../hooks/useFirestore'
 import styles from './home.module.css'
 
 export default function TransactionList({transactions}) {
+  const {deleteDocument} = useFirestore('transactions')
+  
   return (
     <ul className={styles.transactions}>
 
-        {transactions.map(transaction => {
+        {transactions.map((transaction , index) => {
+          
             return (
                 <li key={transaction.id}>
-                    <p className={styles.name}>{transaction.name}</p>
+                    <p className={styles.name}>{index + 1}. {transaction.name}</p>
+                    
                     <p className={styles.amount}>Rs. {transaction.amount}</p>
+
+                    <button onClick={() => deleteDocument(transaction.id)}>X</button>
                 </li>
             )
         })}
