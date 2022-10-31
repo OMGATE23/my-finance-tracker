f41e0a17eded726e5b9d9af8b9b642b0242312ad
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-31 21:59:21 +0530

    feat: added orderBy arguments

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,12 +7,17 @@ import TransactionList from './TransactionList'
 
 export default function Home() {
   const {user} = useAuthContext()
-  const {documents, error} = useCollection('transactions', ['uid' , '==' , user.uid])
+  const {documents, error} = useCollection(
+    'transactions', 
+    ['uid' , '==' , user.uid],
+    ['createdAt' , 'desc']
+    )
 
   
   return (
     <div className={styles.container}>
       <div className={styles.content}>
+        {user && <p>Welcome, {user.displayName}!</p>}
         {error ? <p>{error}</p> : null}
         {documents ? <TransactionList transactions = {documents} /> : null}
       </div>
