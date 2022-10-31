e1f7127af27098c8e8541b6f41620767155879db
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-31 16:08:36 +0530

    feat: added TransactionList page

---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './home.module.css'
+
+export default function TransactionList({transactions}) {
+  return (
+    <ul className={styles.transactions}>
+
+        {transactions.map(transaction => {
+            return (
+                <li key={transaction.id}>
+                    <p className={styles.name}>{transaction.name}</p>
+                    <p className={styles.amount}>Rs. {transaction.amount}</p>
+                </li>
+            )
+        })}
+
+    </ul>
+  )
+}
