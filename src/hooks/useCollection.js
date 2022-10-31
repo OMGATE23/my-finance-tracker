0cbefb7a782f8fd9ceec861394d2b7db433decd7
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-31 21:56:52 +0530

    feat: added orderBy function for documents

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,17 +1,22 @@
 import { useState , useEffect, useRef } from "react";
 import { projectFirebase } from "../firebase/config";
 
-export const useCollection = (collection , _query) => {
+export const useCollection = (collection , _query , _orderBy) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
 
     const query = useRef(_query).current
+    const orderBy = useRef(_orderBy).current
 
     useEffect(() => {
         let ref = projectFirebase.collection(collection)
 
         if(query){
             ref = ref.where(...query)
+        }
+
+        if(orderBy){
+            ref = ref.orderBy(...orderBy)
         }
 
         let unsub = ref.onSnapshot((snapshot) => {
@@ -31,7 +36,7 @@ export const useCollection = (collection , _query) => {
         return () => unsub()
 
 
-    } ,[collection , query])
+    } ,[collection , query , orderBy])
 
     return {documents, error}
 }
