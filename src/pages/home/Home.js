fc13ec7e748fd87aa76318797fb12e0a33acd2b0
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-10-31 16:06:22 +0530

    feat: added Home page transaction list and form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import styles from './home.module.css'
+import TransactionForm from './TransactionForm'
+import {useAuthContext} from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection'
+import TransactionList from './TransactionList'
 
 export default function Home() {
+  const {user} = useAuthContext()
+  const {documents, error} = useCollection('transactions', ['uid' , '==' , user.uid])
+
+  
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {error ? <p>{error}</p> : null}
+        {documents ? <TransactionList transactions = {documents} /> : null}
+      </div>
+
+      <div className={styles.sidebar}>
+        
+        <TransactionForm uid= {user.uid}/>
+      </div>
+    </div>
   )
 }
